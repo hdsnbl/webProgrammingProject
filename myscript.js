496ff5df3d233e68f4d8bac999ff528f1072dca3
hdsnbl <115823910+hdsnbl@users.noreply.github.com>
2023-12-05 21:02:09 -0600

    Delete myscript.js

--- myscript.js
+++
@@ -1,8 +0,0 @@
-function clearForm() {
-  document.getElementById("productName").value = "";
-  document.getElementById("warehouseCity").value = "";
-  document.getElementById("minQuantity").value = "";
-  document.getElementById("maxQuantity").value = "";
-  document.getElementById("minPrice").value = "";
-  document.getElementById("maxPrice").value = "";
-}
